7787e842afd52ca506827a7cbf5e75d1bea76b55
gregoire <gregoire.becue@pm.me>
2026-09-24 10:58:00 +0200

    feat: add more tasks and update task list component

--- src/components/taskContainer.jsx
+++ src/components/taskContainer.jsx
@@ -8,9 +8,10 @@ import { useState } from "react";
 // Ce composant est utilisé pour afficher l'intégralité de la fonctionalité de Tache.
 export function TaskContainer() {
   const [todoItems, setTodoItems] = useState([
-    { id: 1, label: "Apprendre TypeScript & Solid JS", done: false },
-    { id: 2, label: "Faire des gauffres au rhum vanille", done: false },
-    { id: 3, label: "Mettre 20/20 à mon CTP", done: false },
+		{ id: 1, label: 'Apprendre TypeScript & Solid JS', done: false },
+		{ id: 2, label: 'Faire des gauffres au rhum vanille', done: false },
+		{ id: 3, label: 'Mettre 20/20 à mon CTP', done: false },
+		{ id: 4, label: 'Publier mon projet sur GitHub', done: true },
   ]);
 
   return (
